995c93bd89c481cdae786f8f335f19026fe4fb7b
Felipe Gomes <felipenascimentogomes@gmail.com>
2021-04-13 21:39:09 -0300

    Desafio 04 - envio completo

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import e, { Request, Response } from "express";
 
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
@@ -6,7 +6,17 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+
+    try {
+      const list = this.listAllUsersUseCase.execute({
+        user_id: String(user_id),
+      });
+
+      return response.json(list);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,19 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("ID is not valid");
+    }
+
+    if (!user.admin) {
+      throw new Error("User does not Administrator");
+    }
+
+    const all = this.usersRepository.list();
+
+    return all;
   }
 }
 
